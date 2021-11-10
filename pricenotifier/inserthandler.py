0eabaf1275d39844aed6d3d55bf4fd1c3627cc15
Simon Alvhage <38974964+simonalvhage@users.noreply.github.com>
2021-11-10 12:07:14 +0100

    Delete pricenotifier directory

--- pricenotifier/inserthandler.py
+++
@@ -1,9 +0,0 @@
-from main import detectservice,checkifexists,updatedbwithnewperc,insertintodb,infoscraper
-
-def scheduler(mail, link, change, password):
-    service = detectservice(link)
-    if checkifexists(mail, link):
-        updatedbwithnewperc(mail,service,link,change,password)
-    else:
-        insertintodb(mail,service,link,change,password)
-        infoscraper(mail,service,link,change)
